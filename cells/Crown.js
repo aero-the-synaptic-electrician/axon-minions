7f90c48a05c719e5a11e66d7549c0e00406ee75a
aero-the-synaptic-electrician <51681361+aero-the-synaptic-electrician@users.noreply.github.com>
2021-12-29 02:45:24 +0000

    Delete cells directory

--- cells/Crown.js
+++
@@ -1,8 +0,0 @@
-import { Cell } from './Cell.js';
-
-export class Crown extends Cell {
-	/** @param {Number} id */
-	constructor(id) {
-		super(id);
-	}
-}
--- cells/Virus.js
+++
@@ -1,8 +0,0 @@
-import { Cell } from './Cell.js';
-
-export class Virus extends Cell {
-	/** @param {Number} id */
-	constructor(id) {
-		super(id);
-	}
-}
